3cae35beb2cebca45ab40a331836da252685f8cc
Joseph Kamuela Costa <kamuela@mail.sfsu.edu>
2016-10-24 17:54:53 +0000

    Removed a test function

--- application/controller/home.php
+++ application/controller/home.php
@@ -19,9 +19,4 @@ class Home extends Controller {
 		require APP . 'view/home/index.php';
 		require APP . 'view/_templates/footer.php';
 	}
-	public function test() {
-		require APP . 'view/_templates/header.php';
-		require APP . 'view/test/test.php';
-		require APP . 'view/_templates/footer.php';
-	}
 }
