4d9dd997bbc954cdfe1b9f04387fa10fe0080e3c
Joseph Kamuela Costa <kamuela@mail.sfsu.edu>
2016-10-24 18:19:13 +0000

    Update DAO.php to reference the correct DAO model

--- application/controller/DAO.php
+++ application/controller/DAO.php
@@ -4,7 +4,7 @@ class DAO extends Controller{
 		//code executes if "submit_search" has been entered
 		if(isset($_POST["submit_search"])){
 			//do searchListings() in model/DAO.php
-			$this->model->searchListings(_POST["terms"]);
+			$this->DAO->searchListings(_POST["terms"]);
 		}
 		//Page that is shown after search has been executed
 		header('location: ' . URL . http_build_query($_POST["terms"]) . '/home/index');
