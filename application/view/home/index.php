63f5884de2110b549ba324e2ecf59c756d300c1b
Ed Young <edwin.w.young@gmail.com>
2016-10-25 15:12:16 -0700

    Edits to the homepage search form

--- application/view/home/index.php
+++ application/view/home/index.php
@@ -1,13 +1,8 @@
 <div id="wrapper">
-	<form id="search" class="home">
-		<input type="text" placeholder="Address, City, or ZIP code" />
-		<select id="rooms">
-			<option value="0">In-law</option>
-			<option value="1">Studio</option>
-			<option value="2">1 Bedroom</option>
-			<option value="3">2 Bedrooms</option>
-			<option value="4">3+ Bedrooms</option>
-		</select>
-		<input type="submit" value="Search" />
-	</form>
+	<div id="search-wrapper">
+		<form id="search" class="home">
+			<input type="text" placeholder="Address, City, or ZIP code" />
+			<input type="submit" value="Search" />
+		</form>
+	</div>
 </div>
