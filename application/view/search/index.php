f1ac9da974aefc94a3d8dee45a3de3c3f4f9be6d
Ed Young <edwin.w.young@gmail.com>
2016-10-28 13:59:35 -0700

    Finished vertical prototype for Milestone 2

--- application/view/search/index.php
+++ application/view/search/index.php
@@ -1,7 +1,7 @@
 <div id="wrapper">
 	<div id="search-wrapper">
 		<form id="search" class="home" action="<?php echo URL; ?>search" method="GET">
-			<input type="text" name="q" placeholder="Enter Address, City, or ZIP code" />
+			<input type="text" name="q" placeholder="Enter City" <?php if (!empty($_GET['q'])) echo "value='".$_GET["q"]."'"; ?> />
 			<input type="submit" class="btn btn-default" value="Search" />
 		</form>
 	</div>
@@ -13,27 +13,41 @@
 					$rent = $row["MonthlyRent"];
 					$address = $row["StreetNo"] . ' ' . $row["StreetName"] . ', ' . $row["City"] . ', CA ' . $row["ZIP"];
 					$bedrooms = $row['Bedrooms'];
-					$bathrooms = $row['Baths'];
+					$baths = $row['Baths'];
 					$sqft = $row['SqFt'];
 					$description = $row['Description'];
 
-					# Listing heading
 					echo "<li class='listing'>\n";
-					echo "<p><span class='rent'>\$$rent</span> <span class='address'>$address</span></p>";
+					
+					# Listing heading
+					echo "  <div class='listing-heading'>\n";
+					echo "		<p class='listing-heading-left'>\n";
+					echo "			<span class='listing-order'>".($i + 1).".</span>\n";
+					echo "			<span class='address'>$address</span>\n";
+					echo "		</p>\n";
+					echo "		<p class='rent listing-heading-right'>\$$rent</p>\n";
+					echo "  </div>\n";
+					
+					# Listing details
+					echo "	<div class='listing-details'>\n";
+					# Photos window
+					echo "		<div class='photos listing-details-left'>\n";
+					echo "    	<img src='". URL ."public/img/placeholder.png' height='150px' width='150px'/>\n";
+					echo "		</div>\n";
 
-					# Main content div (thumbnails to the left)
-					echo "<div class='photos'></div>";
-					echo "<div class='listing-details'><ul>";
-					echo "<li class='bedrooms'>$bedrooms bedrooms</li>";
-					echo "<li class='bathrooms'>$bathrooms baths</li>";
-					echo "<li class='sqft'>$sqft square feet</li>";
-					echo "</ul></div>";
-
-					# Short blurb
+					
+					# Listing summary
+					echo "		<div class='listing-details-right'>\n";
+					echo "			<ul>\n";
+					echo "				<li class='bedrooms'>$bedrooms bedrooms</li>\n";
+					echo "				<li class='baths'>$baths baths</li>\n";
+					echo "			</ul>\n";
 					if (!empty($description)) {
-						echo "<p class='description'>$description</p>";
+						echo "			<p class='description'>$description</p>\n";
 					}
+					echo "		</div>\n";
 
+					echo "  </div>\n";
 					echo "</li>\n";
 				}
 			?>
