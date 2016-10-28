7321a217bfa7303b2f2c18811f41d17090c98c64
Ed Young <edwin.w.young@gmail.com>
2016-10-28 12:41:17 -0700

    Added form instructions on homepage

--- application/view/home/index.php
+++ application/view/home/index.php
@@ -1,7 +1,7 @@
 <div id="wrapper" class="home">
 
-	<div id="lede"><h1>Find Apartments and Homes for Rent</h1></div>
 	<div id="search-wrapper" class="home">
+		<div id="lede"><h1>Find Apartments and Homes for Rent</h1></div>
 		<form id="search" class="home" method="get" action="<?php echo URL; ?>search">
 			<input type="text" name="q" placeholder="Enter Address, City, or ZIP code" />
 			<input type="submit" class="btn btn-default" value="Search" />
