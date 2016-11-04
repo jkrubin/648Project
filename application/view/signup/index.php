3225923fea4134816f06fa6484c6280f6bc6e49e
Yuning Hong <kitmenghong@gmail.com>
2016-11-03 17:18:55 -0700

    created two new pages for login and sighup

---
+++ application/view/signup/index.php
@@ -0,0 +1,16 @@
+<div id="wrapper" class="signup">
+
+	<div id="search-wrapper" class="signup">
+		<div id="lede"><h1>signup</h1></div>
+                <form class="home" method="post" action="">
+                    <center><input type="email" name="email" placeholder="Email Address" required="@" /></center><br>
+                    <center><input type="text" name="firstname" placeholder="First Name" required /></center><br>
+                    <center><input type="text" name="lastname" placeholder="Last Name" required /></center><br>
+                    <center><input type="password" name="password" placeholder="Password" required /></center><br>
+                    <center><input type="password" name="repass" placeholder="Retype Password" required /></center><br>
+                    <center><label>By registering, I accept the RentSFSU.com <?php echo URL; ?> <a href="#">Terms of Service</a>.</label></center><br>
+                    
+                    <center><input type="submit" class="btn btn-default" value="Create Account"/></center>
+		</form>
+	</div>
+</div>
