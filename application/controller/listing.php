3254c29f6fdb8a1fb6347612700d112a0f4c6344
Martha <gomezlet@mail.sfsu.edu>
2016-11-05 08:42:26 +0000

    Create a Listing page - still needs changes

---
+++ application/controller/listing.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * Class Listing
+ *
+ * Please note:
+ * Don't use the same name for class and method, as this might trigger an (unintended) __construct of the class.
+ * This is really weird behaviour, but documented here: http://php.net/manual/en/language.oop5.decon.php
+ *
+ */
+class Listing extends Controller {
+	/**
+	 * PAGE: index
+	 * This method handles what happens when you move to http://yourproject/home/index (which is the default page btw)
+	 */
+	public function index() {
+		// load views
+                require APP . 'view/_templates/header.php';
+		require APP . 'view/listing/index.php';
+		require APP . 'view/_templates/footer.php';
+
+	}
+}
