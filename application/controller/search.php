c154b9b4a652bf5f1e818593d46ffcbf23affc97
Ed Young <edwin.w.young@gmail.com>
2016-11-05 17:03:17 -0700

    Fixed code style issues in application/controller/search.php

--- application/controller/search.php
+++ application/controller/search.php
@@ -30,17 +30,17 @@ class Search extends Controller {
 		$unsplit_cities = "";
 		$temp_array = array();
 
-		foreach($assoc_array as $i => $row){
-			if(!(in_array($row["City"], $temp_array))) {
-				$unsplit_cities .= $unsplit_cities." ".strtolower($row["City"]);
+		foreach ($assoc_array as $i => $row) {
+			if (!(in_array($row["City"], $temp_array))) {
+				$unsplit_cities .= $unsplit_cities . " " . strtolower($row["City"]);
 				array_push($temp_array, $row["City"]);
 			}
 		}
 
 		$cities = preg_split("/ /", $unsplit_cities);
 
-		$streets = array("way", "street", "road", "court", "boulevard", "blvd", "place", "avenue", "ave", "beach", "bch", "causeway", "circle", "drive", "dr", 
-						 "expressway", "heights", "ht", "junction", "jct", "lane", "ln", "plaza", "rd", "st", "ct", "square", "sq");
+		$streets = array("way", "street", "road", "court", "boulevard", "blvd", "place", "avenue", "ave", "beach", "bch", "causeway", "circle", "drive", "dr",
+				"expressway", "heights", "ht", "junction", "jct", "lane", "ln", "plaza", "rd", "st", "ct", "square", "sq");
 		$splitQuery = array();
 		$sortedQuery = array();
 
@@ -51,48 +51,49 @@ class Search extends Controller {
 		 */
 		if (!empty($_GET)) {
 			$temp = preg_split("/ /", $_GET["q"]);
-			for($i = 0; $i < count($temp); $i++){
-				if(in_array(strtolower($temp[$i]), $cities)){
+			for ($i = 0; $i < count($temp); $i++) {
+				if (in_array(strtolower($temp[$i]), $cities)) {
 					array_push($splitQuery, strtolower($temp[$i]));
-				}else if(in_array(strtolower($temp[$i]), $cities) && (count($temp) >= 2)){
-					array_push($splitQuery, strtolower($temp[$i])." ".(strtolower($temp[$i + 1])));
+				} else if (in_array(strtolower($temp[$i]), $cities) && (count($temp) >= 2)) {
+					array_push($splitQuery, strtolower($temp[$i]) . " " . (strtolower($temp[$i + 1])));
 					$i++;
-				}elseif(in_array(strtolower($temp[$i]), $streets) && (count($temp) >= 2)){
-					array_push($splitQuery, strtolower($temp[$i - 1])." ".strtolower($temp[$i]));
+				} else if (in_array(strtolower($temp[$i]), $streets) && (count($temp) >= 2)) {
+					array_push($splitQuery, strtolower($temp[$i - 1]) . " " . strtolower($temp[$i]));
 					$i++;
-				}else{
+				} else {
 					array_push($splitQuery, strtolower($temp[$i]));
 				}
 			}
 
 
-		/*
-		 *foreach look takes each item in $splitQuery and catagorizes it
-		 *If $key is a 5 digit number starting with a 9 then it will get added into 'zip'
-		 *If $key is a number, but not a zip code, then it will push added to 'stno'
-		 *if $key is a string that is in the array $cities, it will be added to 'cities'
-		 *otherwise it will be added 'stadd'
-		 */
+			/*
+			 *foreach look takes each item in $splitQuery and catagorizes it
+			 *If $key is a 5 digit number starting with a 9 then it will get added into 'zip'
+			 *If $key is a number, but not a zip code, then it will push added to 'stno'
+			 *if $key is a string that is in the array $cities, it will be added to 'cities'
+			 *otherwise it will be added 'stadd'
+			 */
 			$i = 0;
-			foreach($splitQuery as $key){
-				if(is_numeric($key)){
-					if(preg_match("/[0-9]{5}/", $key) && preg_match("/^9/", $key) && (($i+1) == count($splitQuery))){
+			foreach ($splitQuery as $key) {
+				if (is_numeric($key)) {
+					if (preg_match("/[0-9]{5}/", $key) && preg_match("/^9/", $key) && (($i + 1) == count($splitQuery))) {
 						$sortedQuery['zip'] = $key;
-					}else{
+					} else {
 						$sortedQuery['stno'] = $key;
 					}
-				}else{
-					if(in_array($key, $cities)){
+				} else {
+					if (in_array($key, $cities)) {
 						$sortedQuery['city'] = $key;
-					}else{
+					} else {
 						$sortedQuery['stadd'] = $key;
 					}
 				}
 				$i++;
 			}
-			
-		return $this->model->get_listings($sortedQuery);
+
+			return $this->model->get_listings($sortedQuery);
 		}
 	}
 }
+
 ?>
