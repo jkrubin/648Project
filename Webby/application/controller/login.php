4e090c7816711239ffdfb3f30991c30b821183ed
scubanuts <scubanuts@Ryan>
2016-11-07 00:50:42 -0800

    Fixed login controller(it works)

--- Webby/application/controller/login.php
+++ Webby/application/controller/login.php
@@ -9,27 +9,28 @@
  *
  */
 class Login extends Controller {
-	/**
-	 * PAGE: index
-	 * This method handles what happens when you move to http://yourproject/home/index (which is the default page btw)
-	 */
-	public function index() {
-		// load views
-		require APP . 'view/_templates/home_header.php';
-		require APP . 'view/login/index.php';
-		require APP . 'view/_templates/footer.php';
-	}
-        
-        public function user_login(): bool {
-            if (isset($_POST['submit'])) {
-                $userMail = $_POST['email'];
-                $userPass = $_POST['password'];
-                if (authenticate_user($userMail, $userPass)){
-                    return true;
-                } else {
-                    return false;
+    /**
+     * PAGE: index
+     * This method handles what happens when you move to http://yourproject/home/index (which is the default page btw)
+     */
+    public function index() {
+
+                if(isset($_POST['submit'])){
+                        $check_login = $this->user_login();
                 }
-            }
+
+        // load views
+        require APP . 'view/_templates/home_header.php';
+        require APP . 'view/login/index.php';
+        require APP . 'view/_templates/footer.php';
+    }
+
+        public function user_login(): array {
+
+            $user_mail = $_POST['email'];
+            $user_pass = $_POST['password'];
+            $array = $this->model->authenticate_user($user_mail,$user_pass);
+
+            return $array;
         }
-            
 }
