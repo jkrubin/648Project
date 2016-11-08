1cbe3df88d5430ef31f233cf39ae7e7c1f057b40
lety <lety@localhost>
2016-11-07 20:48:17 -0800

    Merge origin/model/add-listing into model/add-listing
    Conflicts:
    	application/view/listing/index.php

--- application/view/listing/index.php
+++ application/view/listing/index.php
@@ -6,9 +6,9 @@
                     <input type="text" name="streetNo" placeholder="Street Number" />
                     <input type="text" name="streetName" placeholder="Street Name" />
                     <input type="text" name="city" placeholder="City" />
-                    <input type="text" name="sqFt" placeholder="Square Feet" />
                     <input type="text" name="zipCode" placeholder="Zip Code" /><br><br>
                     <input type="text" name="description" id="description" placeholder="Description" /><br><br>
+                    <input type="text" name="sqFt" placeholder="Square Feet" />
                     <input type="text" name="monthlyRent" placeholder="Monthly Rent" />
                     <input type="text" name="deposit" placeholder="Deposit" />
                     <input type="text" name="petDeposit" placeholder="Pet Deposit" />
@@ -23,9 +23,9 @@
                     <input type="checkbox" name="furnished"/>Comes Furnished<br><br>
                     <input type="text" name="bedrooms" id="bedrooms" placeholder="Number of Rooms" />
                     <input type="text" name="baths" id="baths" placeholder="Number of Bathrooms" />
-                    <input type="text" name="startDate" placeholder="Available From (Date)" />
-                    <input type="text" name="endDate" placeholder="Available To (Date)" /><br><br>
-                    <input type="button" name="images" value="Upload Images of the Listing"/><br><br>
+                    <input type="date" name="startDate" placeholder="Available From (Date)" />
+                    <input type="date" name="endDate" placeholder="Available To (Date)" /><br><br>
+                    <input type="file" name="images" value="Upload Images of the Listing"/><br><br>
                     <input type="checkbox" name="agree"/>I agree to the terms
                     <input type="submit" class="btn btn-default" name ="submit_listing" value="Create Listing" />
 		</form>
