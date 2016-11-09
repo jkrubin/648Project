029b01672760ca19f0a7dfd07fce7891b5041651
lety <lety@localhost>
2016-11-09 15:42:49 -0800

    Form changes for Dashboard and Create Listing page: formatting fixes for the form and heading scss working

--- application/view/dashboard/index.php
+++ application/view/dashboard/index.php
@@ -1,52 +1,52 @@
-<div> <header id="page-title"> Landlord Dashboard </header> </div>
+<div id="page-title"> <h2> Landlord Dashboard </h2> </div>
 
 <div id="wrapper">
-	<div id="dashboard-wrapper">
-		<ul id="listings">
-			<?php
-				foreach ($listings as $i => $row) {
+    <div id="dashboard-wrapper">
+        <ul id="listings">
+            <?php
+            foreach ($listings as $i => $row) {
 
-					$rent = $row["MonthlyRent"];
-					$address = $row["StreetNo"] . ' ' . $row["StreetName"] . ', ' . $row["City"] . ', CA ' . $row["ZIP"];
-					$bedrooms = $row['Bedrooms'];
-					$baths = $row['Baths'];
-					$sqft = $row['SqFt'];
-					$description = $row['Description'];
+                $rent = $row["MonthlyRent"];
+                $address = $row["StreetNo"] . ' ' . $row["StreetName"] . ', ' . $row["City"] . ', CA ' . $row["ZIP"];
+                $bedrooms = $row['Bedrooms'];
+                $baths = $row['Baths'];
+                $sqft = $row['SqFt'];
+                $description = $row['Description'];
 
-					echo "<li class='listing'>\n";
-					
-					# Listing heading
-					echo "  <div class='listing-heading'>\n";
-					echo "		<p class='listing-heading-left'>\n";
-					echo "			<span class='listing-order'>".($i + 1).".</span>\n";
-					echo "			<span class='address'>$address</span>\n";
-					echo "		</p>\n";
-					echo "		<p class='rent listing-heading-right'>\$$rent</p>\n";
-					echo "  </div>\n";
-					
-					# Listing details
-					echo "	<div class='listing-details'>\n";
-					# Photos window
-					echo "		<div class='photos listing-details-left'>\n";
-					echo "    	<img src='". URL ."public/img/placeholder.png' height='150px' width='150px'/>\n";
-					echo "		</div>\n";
+                echo "<li class='listing'>\n";
 
-					
-					# Listing summary
-					echo "		<div class='listing-details-right'>\n";
-					echo "			<ul>\n";
-					echo "				<li class='bedrooms'>$bedrooms bedrooms</li>\n";
-					echo "				<li class='baths'>$baths baths</li>\n";
-					echo "			</ul>\n";
-					if (!empty($description)) {
-						echo "			<p class='description'>$description</p>\n";
-					}
-					echo "		</div>\n";
+                # Listing heading
+                echo "  <div class='listing-heading'>\n";
+                echo "		<p class='listing-heading-left'>\n";
+                echo "			<span class='listing-order'>" . ($i + 1) . ".</span>\n";
+                echo "			<span class='address'>$address</span>\n";
+                echo "		</p>\n";
+                echo "		<p class='rent listing-heading-right'>\$$rent</p>\n";
+                echo "  </div>\n";
 
-					echo "  </div>\n";
-					echo "</li>\n";
-				}
-			?>
-		</ul>
-	</div>
+                # Listing details
+                echo "	<div class='listing-details'>\n";
+                # Photos window
+                echo "		<div class='photos listing-details-left'>\n";
+                echo "    	<img src='" . URL . "public/img/placeholder.png' height='150px' width='150px'/>\n";
+                echo "		</div>\n";
+
+
+                # Listing summary
+                echo "		<div class='listing-details-right'>\n";
+                echo "			<ul>\n";
+                echo "				<li class='bedrooms'>$bedrooms bedrooms</li>\n";
+                echo "				<li class='baths'>$baths baths</li>\n";
+                echo "			</ul>\n";
+                if (!empty($description)) {
+                    echo "			<p class='description'>$description</p>\n";
+                }
+                echo "		</div>\n";
+
+                echo "  </div>\n";
+                echo "</li>\n";
+            }
+            ?>
+        </ul>
+    </div>
 </div>
--- application/view/listing/index.php
+++ application/view/listing/index.php
@@ -1,33 +1,33 @@
 <div id="page-title"> <h2> Create a Listing </h2> </div>
 
-<div class="listing">
+<div class="add-listing">
     <div>
         <form action="<?php echo URL; ?>listing/addListing" method="POST">
             <input type="text" name="streetNo" placeholder="Street Number" width="22%"/>
             <input type="text" name="streetName" placeholder="Street Name" />
             <input type="text" name="city" placeholder="City" />
             <input type="text" name="zipCode" placeholder="Zip Code" /><br><br>
-            <textarea cols="126" rows="3" name="description" placeholder="Description"></textarea><br><br>
-            <input type="text" name="sqFt" placeholder="Square Feet" />
             <input type="text" name="monthlyRent" placeholder="Monthly Rent" />
             <input type="text" name="deposit" placeholder="Deposit" />
+            <input type="text" name="bedrooms" id="bedrooms" placeholder="Number of Rooms" />
+            <input type="text" name="baths" id="baths" placeholder="Number of Bathrooms" /><br><br>
+            <textarea cols="126" rows="3" name="description" placeholder="Description"></textarea><br><br>
+            <input type="text" name="sqFt" placeholder="Square Feet" />
             <input type="text" name="petDeposit" placeholder="Pet Deposit" />
             <input type="text" name="keyDeposit" placeholder="Key Deposit" /><br><br>
-            <input type="checkbox" name="electricity"/>Electrical
-            <input type="checkbox" name="internet"/>Internet
-            <input type="checkbox" name="water"/>Water
-            <input type="checkbox" name="gas"/>Gas
-            <input type="checkbox" name="pets"/>Allow Pets
-            <input type="checkbox" name="smoking"/>Allow Smoking
-            <input type="checkbox" name="television"/>Television
-            <input type="checkbox" name="furnished"/>Comes Furnished<br><br>
-            <input type="text" name="bedrooms" id="bedrooms" placeholder="Number of Rooms" />
-            <input type="text" name="baths" id="baths" placeholder="Number of Bathrooms" />
             Start date:
             <input type="date" name="startDate" placeholder="Available From (Date)" />
             End date:
             <input type="date" name="endDate" placeholder="Available To (Date)" /><br><br>
             Add images:<br><br>
+            <input type="checkbox" name="electricity"/> <label>Electrical</label>
+            <input type="checkbox" name="internet"/> <label>Internet</label>
+            <input type="checkbox" name="water"/> <label>Water</label>
+            <input type="checkbox" name="gas"/> <label>Gas</label>
+            <input type="checkbox" name="pets"/> <label>Allow Pets</label>
+            <input type="checkbox" name="smoking"/> <label>Allow Smoking</label>
+            <input type="checkbox" name="television"/> <label>Television</label>
+            <input type="checkbox" name="furnished"/> <label>Furnished</label><br><br>
             <input type="file" name="images" value="Upload Images of the Listing"/><br><br>
             <input type="checkbox" name="agree"/>I agree to the terms
             <input type="submit" class="btn btn-default" name ="submit_listing" value="Create Listing" />
