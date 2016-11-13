c02194101415a0f0dcc17e9c084730b711556836
Yuning Hong <kitmenghong@gmail.com>
2016-11-12 22:11:21 -0800

    modal now can accessed through header and home_header. login.php & signup.php will be deleted in future commit when modal is confirmed to be working properly.

--- application/view/_templates/footer.php
+++ application/view/_templates/footer.php
@@ -11,7 +11,9 @@
         
         <script src="<?php echo URL; ?>js/bootstrap/tab.js"></script>  
         
-        <script src="<?php echo URL; ?>js/bootstrap/modal.js"></script>      
+        <script src="<?php echo URL; ?>js/bootstrap/modal.js"></script> 
+        
+        <script src="<?php echo URL; ?>js/tab-select.js"></script> 
 
 </body>
 </html>
--- application/view/_templates/home_header.php
+++ application/view/_templates/home_header.php
@@ -31,10 +31,74 @@
 			<div class="navbar-right">
 				<ul>
 					<a href="<?php echo URL; ?>"><li class="btn btn-default">Add listing</li></a>
-					<a href="<?php echo URL; ?>signup"><li>Sign up</li></a>
-					<a href="<?php echo URL; ?>login"><li>Log in</li></a>
-				</ul>
+                                        <a href="#signup" data-toggle="modal" data-target=".bs-modal-sm" onClick="tab_select(this.hash)"><li>Register</li></a>
+                                        <a href="#login" data-toggle="modal" data-target=".bs-modal-sm" onClick="tab_select(this.hash)"><li>Log in</li></a>
+				</ul>                                
 			</div>
 		</nav>
 		<div id="disclaimer">SFSU/FAU/Fulda Software Engineering Project, Fall 2016.  For Demonstration Only</div>
 	</header>
+        
+        <!-- Modal -->
+        <div class="modal bs-modal-sm" id="myModal" tabindex="-1" role="dialog" aria-labelledby="mySmallModalLabel" aria-hidden="true">
+                <div class="modal-dialog modal-sm">
+                        <div class="modal-content"><br>
+                                <!-- Modal Tabs-->
+                                <div class="bs-example bs-example-tabs">
+                                        <ul class="nav nav-tabs" id="myTab">
+                                                <li class="active"><a href="#signup" data-toggle="tab">Sign up</a></li>
+                                                <li class=""><a href="#login" data-toggle="tab" onclick="tab_select(location.hash)">Log in</a></li>                       
+                                        </ul>
+                                </div>
+                                <!-- Modal Forms-->
+                                <div class="modal-body">
+                                        <div id="myTabContent" class="tab-content">       
+                                                <div class=" tab-pane" id="login">
+                                                        <form id="form-wrapper" method="post" action="">
+                                                                <input class="form-input" type="email" name="email" placeholder="Email Address" required />
+                                                                <input class="form-input" type="password" name="password" placeholder="Password" required /><br/>
+                                                                <input type="submit" name="login" class="form-input btn btn-default" value="Log in"/>
+                                                        </form>                                
+
+                                                        <?php
+                                                            if(isset($_POST['login'])){
+                                                                if($check_login['status'] == 'success'){
+                                                                    echo "<center>log in successful =D</center>";
+                                                                } elseif($check_login['status'] == 'error') {
+                                                                    echo "<center>" . $check_login['message'] . "</center>";
+                                                                }
+                                                            }
+                                                        ?>
+                                                </div>
+
+                                                <div class=" tab-pane active" id="signup">
+                                                        <form id="form-wrapper" method="post" action="">
+                                                                <input class="form-input" type="email" name="email" placeholder="Email Address" required="@" />
+                                                                <input class="form-input" type="text" name="firstname" placeholder="First Name" required />
+                                                                <input class="form-input" type="text" name="lastname" placeholder="Last Name" required />
+                                                                <input class="form-input" type="password" name="password" placeholder="Password" required />
+                                                                <input class="form-input" type="password" name="repass" placeholder="Retype Password" required />
+                                                                <label class="form-input">By registering, I accept the RentSFSU.com <a href="#">Terms of Service</a>.</label>
+                                                                <input type="submit" name="signup" class="btn btn-default" value="Sign up"/>            
+                                                        </form>  
+
+                                                    <?php
+                                                        if(isset($_POST['signup'])){
+                                                            if($check){
+                                                                echo "<center>success</center>";
+                                                            } else {
+                                                                echo "<center>unsuccess</center>";
+                                                            }
+                                                        }
+                                                    ?>
+                                                </div>
+                                        </div>
+                                </div>
+                        </div>
+                </div>
+        </div>
+</body>
+
+
+
+
