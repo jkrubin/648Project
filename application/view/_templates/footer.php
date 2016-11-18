39551b57e66a42e0e7dbf0237663b40d5a81790f
Ed Young <edwin.w.young@gmail.com>
2016-11-17 20:27:31 -0800

    Moved JS url variable to header

--- application/view/_templates/footer.php
+++ application/view/_templates/footer.php
@@ -1,8 +1,5 @@
 
-<!-- define the project's URL (to make AJAX calls possible, even when using this in sub-folders etc) -->
-<script>
-	var url = "<?php echo URL; ?>";
-</script>
+
 
 <!-- our JavaScript -->
 <script src="<?php echo URL; ?>js/application.js"></script>
