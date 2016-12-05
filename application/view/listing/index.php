af080c4ef4cca750abaebc5525689f5894569310
lety <lety@localhost>
2016-12-05 15:27:56 -0800

    Adding message center
    merging branches

--- application/view/listing/index.php
+++ application/view/listing/index.php
@@ -1,8 +1,10 @@
+
+
 <div id="page-title"> <h2> Create a Listing </h2> </div>
 
 <div class="add-listing">
     <div>
-        <form data-toggle="validator" role="form" action="<?php echo URL; ?>listing/addlisting" method="POST"><br>
+        <form data-toggle="validator" role="form" method="post" enctype="multipart/form-data" action="<?php echo URL; ?>listing/addlisting"><br>
             <text id="showLabelSTNo">Street Number</text>
             <text id="showLabelSTName">Street Name</text>
             <text id="showLabelCity">City</text>
@@ -150,8 +152,10 @@
     <input type="checkbox" id="television" name="television"/> <label for="television">Television</label>
     <input type="checkbox" id="furished" name="furnished"/> <label for="furnished">Furnished</label><br><br><br>
     Add images:<br><br>
-    <input type="file" name="images" value="Upload Images of the Listing"/><br><br><br>
+    <input type="file" id="images" name="images"/><br><br><br>
     <input type="checkbox" name="agree"/> By checking, I accept the <a href="#">Terms of Service</a>
     <input type="submit" id="button" class="btn btn-default" name ="submit_listing" value="Create Listing" />
+ 
 </form>
 </div>
+
