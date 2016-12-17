ba6c8c1e2191e6798e4a1f5e60a2a6d22a09c652
lety <lety@localhost>
2016-12-16 16:27:23 -0800

    message center - fix to the reply form

--- application/view/message_center/index.php
+++ application/view/message_center/index.php
@@ -71,16 +71,45 @@
 		<div class="modal-dialog">
 			<div class="modal-content"><br>
 				<div class="modal-header text-center">
-					<h class="modal-title">Reply to Potential rentee</h>
+					<button type="button" class="close" data-dismiss="modal" aria-label="Close">
+						<span aria-hidden="true">&times;</span>
+					</button>
+					<h class="modal-title">Reply to Potential Rentee</h>
 				</div>
 				<!-- Modal Forms-->
 				<div class="modal-body">
 					<div class=" tab-pane active">
-						<form id="form-wrapper" method="post" action="" data-toggle="validator">
+						<form id="form-wrapper" method="post" action="<?php echo URL."api/sendmessage($_POST[senderId])";?>" data-toggle="validator">
 							<div class="form-group row">
 
+								<img class="col-sm-4" src='<?php echo URL; ?>public/img/placeholder.png' height='150px' width='150px'/>
+
 								<div class="col-sm-8">
-                                    <input class="form-input form-control" type="text" name="subject" value="<?php echo$title?>" required="">
+									<div class="address">
+										<?php echo "123 abc street, San Francisco";//$_POST['address'] ?></p>
+										
+									</div>
+									<div class="rental-details">
+										<p><span class="bedrooms">
+											<?php
+                                                                                                echo "1 room";
+//												echo $_POST['bedrooms'] . " bedroom";
+//												if($_POST['bedrooms'] > 1){
+//												    echo "s";
+//												}
+											?>
+										</span> &nbsp; 
+										<span class="baths">
+											<?php 	
+                                                                                                echo "1 bath";
+//												echo $_POST['baths'] . " bath";
+//												if($_POST['baths'] > 1){
+//												    echo "s";
+//												} 
+											?>
+										</span></p>
+									</div>
+									<input class="form-input form-control" type="text" name="subject" value="<?php echo$title?>" required="">
 									<div class="help-block with-errors"></div>
 								</div>
 							</div>
